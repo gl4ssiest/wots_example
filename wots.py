9e7417e3b4ac986ccac799703e4cf0b50f75d6ab
Andres Rodriguez <josh.rodriguez.b@gmail.com>
2019-01-12 00:42:13 +0100

    Much better prints

--- wots.py
+++ wots.py
@@ -6,11 +6,11 @@ import time
 from os import urandom
 
 import sys
-message="AAAAAAAAAABBBBBBBBBCCCCCCCCC"
+message=raw_input("Please enter message: ")
 
 def random_key(x, n=32): 
     key = hexlify(urandom(n)) #returns a 256 bit hex encoded (64 bytes) random number
-    print "\nPrivate key value " , x , " : ", key
+    print "\n------------\nPrivate key value " , x , " : \t", key
     return key
 
 def sha256(message):
@@ -24,61 +24,58 @@ def random_wkey(w=8, verbose=0):      #create random W-OTS keypair
     priv = []
     pub = []
     print "=============== Public - Private key pair generation ================"
-    raw_input("Press enter to continue....")    
+    raw_input("\nPress enter to continue....")    
     
     for x in range(256/w):
         a = random_key(x)
         priv.append(a)
-        print "\n\t",2**w,"Hashing (SHA-256) iterations ",
+        print "\n\t After 3 doritos and ",2**w,"Hashing (SHA-256) iterations ... \n"
         for y in range(2**w):              
             a = sha256(a)
-            print ">",
+            #print ">",
         pub.append(a)
-        print "\n\nPublic key value " , x , " : ", a 
+        print "Public key value " , x , " : \t", a,
     
-    raw_input("Press enter to continue....")    
+    raw_input("\nPress enter to continue....")    
     return priv, pub 
 
 def sign_wkey(priv, message):      
     print "\n=============== Signature (privatekey, message)==============="
-    raw_input("Press enter to continue....")  
+    raw_input("\nPress enter to continue....")    
     signature = []
     bin_msg = unhexlify(sha256(message))
-    print "\n\t",bin_msg
 
     for y in range(len(priv)):
         s = priv[y]   
         messageHashPieceValue = ord(bin_msg[y:y+1])
-        print "\n\t\t 256 -",messageHashPieceValue,"=",256 - messageHashPieceValue,"Hashing (SHA-256) iterations over PRIVATE KEY piece",y 
+        print "Signature value",y," :  256 -",messageHashPieceValue,"=",256 - messageHashPieceValue,"Hashing (SHA-256) iterations over PRIVATE KEY piece",y 
         for x in range(256 - messageHashPieceValue):
-            print ">",
+            #print ">",
             s = sha256(s)
         signature.append(s)
     return signature
 
 def verify_wkey(signature, message, pub):
     print "\n=============== Verify (signature, message, publickey) ==============="
-    raw_input("Press enter to continue....")  
-
+    raw_input("\nPress enter to continue....")    
     verify = []
     bin_msg = unhexlify(sha256(message))
-    print "\n\t",bin_msg
-    
+   
     for x in range(len(signature)):
         a = signature[x]
         
         messageHashPieceValue = ord(bin_msg[x:x+1])
-        print "\n\t\t",messageHashPieceValue,"Hashing (SHA-256) iterations over SIGNATURE piece",x                                             
+        print "Verifying signature value ",x ," : ", messageHashPieceValue,"Hashing (SHA-256) iterations over this SIGNATURE piece"                                             
         for z in range(messageHashPieceValue):
                 a=sha256(a)
-                print ">",
+                #print ">",
         # a = sha256(a + ".")                                # is the final hash, separate so can be changed..
         verify.append(a)
   
     if pub != verify:
         return False
 
-    return True
+    return verify
 
 
 priv, pub = random_wkey()
@@ -123,5 +120,10 @@ result = verify_wkey(sign,message,pub)
 
 print "\n=============== Verification result ==============="
 print "\nThe signature test is ....... ",
-raw_input("?????")  
-print "\n",result
+raw_input("??????????????")    
+print "Verification[0]:\t",result[0],"\nPub[0]:\t",pub[0]
+print "Verification[1]:\t",result[1],"\nPub[1]:\t",pub[1]
+print "Verification[2]:\t",result[2],"\nPub[2]:\t",pub[2]
+print "Verification[3]:\t",result[3],"\nPub[3]:\t",pub[3]
+print "..."
+print "Verification[31]:\t",result[31],"\nPub[31]:\t",pub[31]
